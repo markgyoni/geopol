3a6d3d1fd4e6990f0242df39141fdfe463ce9c22
IoMARKoI <markgyoni@gmail.com>
2023-01-20 12:07:18 +0100

    fix

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import factionsPNG from '@site/static/img/factions.png';
+import weaponsPNG from '@site/static/img/weaponsv3.png';
+const FeatureList = [
+  {
+    title: 'Factions',
+    img: factionsPNG,
+    description: (
+      <>
+        placeholder
+      </>
+    ),
+  },
+  {
+    title: 'Weapons',
+    img: weaponsPNG,
+    description: (
+      <>
+        placeholder
+      </>
+    ),
+  },
+  {
+    title: 'Placeholder',
+    img: factionsPNG,
+    description: (
+      <>
+        placeholder
+      </>
+    ),
+  },
+];
+
+function Feature({img, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img src={img}/>
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
